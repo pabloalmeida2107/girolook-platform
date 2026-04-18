717d92c9a13db64c3ca67a7cad191da5d7570346
Pablo Almeida <pablo.mengo66@gmail.com>
2026-04-18 15:18:34 -0300

    fix: forcando https no swagger e tratando mixed content

---
+++ GiroLook/src/main/java/Girolook/com/GiroLook/config/OpenAPIConfig.java
@@ -0,0 +1,20 @@
+package Girolook.com.GiroLook.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class OpenAPIConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .servers(List.of(
+                        new Server().url("https://girolook-platform-production.up.railway.app")
+                ));
+    }
+}
